9d1134454fca4a0b18756b8397dab6cae1c82f52
bhautik <jayvirumax345@gmail.com>
2021-11-06 14:26:23 +0530

    first commit

--- src/main/java/com/bk/examportal/controller/UserController.java
+++ src/main/java/com/bk/examportal/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.bk.examportal.entity.UserRole;
 import com.bk.examportal.userservice.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
